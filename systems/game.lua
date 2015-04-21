c02b636d9977686bb16c9ba2b68abb2ab0d47cb8
Wade Rauschenbach <lt.ven0mI@gmail.com>
2015-04-21 20:54:34 +1000

    Removed unused code

--- systems/game.lua
+++ systems/game.lua
@@ -26,7 +26,6 @@ function game.onStateLoad()
 	love.graphics.setFont(game.scoreFont)
 	game.blipEffects[1] = love.audio.newSource("/assets/audio/pongBlip2.wav", "static")
 	game.blipEffects[2] = love.audio.newSource("/assets/audio/pongBlip2.wav", "static")
-	-- game.addPlayer()
 	game.startGame()
 end
 
@@ -41,22 +40,6 @@ end
 function game.update(dt)
 	local width, height = love.graphics.getDimensions()
 	if game.myID > 0 and game.myID < 3 then
-		-- local player = game.players[game.myID]
-		-- local moveAmount = 0
-		-- if game.myID == 1 or game.myID == 2 then
-		-- 	moveAmount = love.keyboard.isDown("up") and -1 or 0
-		-- 	moveAmount = love.keyboard.isDown("down") and 1 or moveAmount
-		-- else
-		-- 	moveAmount = love.keyboard.isDown("left") and -1 or 0
-		-- 	moveAmount = love.keyboard.isDown("right") and 1 or moveAmount
-		-- end
-
-		-- if player then
-		-- 	player.pos = player.pos + moveAmount*game.paddleSpeed*dt
-		-- 	if player.pos-game.paddleLength/2 < 0 then player.pos = game.paddleLength/2 end
-		-- 	if player.pos+game.paddleLength/2 > width then player.pos = width-game.paddleLength/2 end
-		-- end
-
 		local player = game.players[game.myID]
 
 		local moveAmount = 0
@@ -70,59 +53,6 @@ function game.update(dt)
 			net.set("player_pos",math.floor(player.pos))
 		end
 	end
-
-	-- if game.ball then
-	-- 	game.ball.pos = game.ball.pos:add(game.ball.vel:mul(game.ballSpeed):mul(dt))
-
-	-- 	local bx, by = game.ball.pos:xy()
-	-- 	if bx+game.ballSize/2 < 0 or bx-game.ballSize/2 > width or by+game.ballSize/2 < 0 or by-game.ballSize/2 > height then game.endGame() end
-
-	-- 	local player = game.players[1]
-	-- 	if player then
-	-- 		if bx-game.ballSize/2 < game.paddlePadding+game.paddleDepth and by-game.ballSize/2 < player.pos+game.paddleLength/2 and by+game.ballSize/2 > player.pos-game.paddleLength/2 then
-	-- 			game.ball.vel.x = positive(game.ball.vel.x)
-	-- 			local ratio = (game.ball.pos.y-player.pos)/(game.paddleLength/4)
-	-- 			game.ball.vel.y = ratio
-	-- 			game.ball.pos.x = game.paddlePadding+game.paddleDepth+game.ballSize/2
-	-- 			game.playSound()
-	-- 		end
-	-- 		if bx+game.ballSize/2 < 0 then game.endGame(1) end
-	-- 	else
-	-- 		if bx-game.ballSize/2 < 0 then
-	-- 			game.ball.vel.x = positive(game.ball.vel.x)
-	-- 			game.ball.pos.x = game.ballSize/2
-	-- 			game.playSound()
-	-- 		end
-	-- 	end
-
-	-- 	player = game.players[2]
-	-- 	if player then
-	-- 		if bx+game.ballSize/2 > width-game.paddlePadding-game.paddleDepth and by-game.ballSize/2 < player.pos+game.paddleLength/2 and by+game.ballSize/2 > player.pos-game.paddleLength/2 then
-	-- 			game.ball.vel.x = negative(game.ball.vel.x)
-	-- 			local ratio = (game.ball.pos.y-player.pos)/(game.paddleLength/4)
-	-- 			game.ball.vel.y = ratio
-	-- 			game.ball.pos.x = width-game.paddlePadding-game.paddleDepth-game.ballSize/2
-	-- 			game.playSound()
-	-- 		end
-	-- 		if bx-game.ballSize/2 > width then game.endGame(2) end
-	-- 	else
-	-- 		if bx+game.ballSize/2 > width then
-	-- 			game.ball.vel.x = negative(game.ball.vel.x)
-	-- 			game.ball.pos.x = width-game.ballSize/2
-	-- 			game.playSound()
-	-- 		end
-	-- 	end
-
-	-- 	if by-game.ballSize/2 < 0 then
-	-- 		game.ball.vel.y = positive(game.ball.vel.y)
-	-- 		game.playSound()
-	-- 	end
-
-	-- 	if by+game.ballSize/2 > height then
-	-- 		game.ball.vel.y = negative(game.ball.vel.y)
-	-- 		game.playSound()
-	-- 	end
-	-- end
 end
 
 function game.draw()
@@ -160,14 +90,6 @@ function game.draw()
 	love.graphics.setLineWidth(lineWidth)
 end
 
-function game.keypressed(key)
-	if key == " " then
-		game.addPlayer()
-	elseif key == "f" then
-		game.removePlayer(#game.players)
-	end
-end
-
 function game.playSound()
 	if game.blipEffects[1]:isPlaying() then
 		game.blipEffects[2]:play()
@@ -196,18 +118,6 @@ function game.startGame()
 	game.ball = {pos=vec2.new(width/2, height/2),vel=vec2.new((math.random(0, 1)-0.5)*2, (math.random(0, 1)-0.5)*2)}
 	if game.players[1] then game.players[1].pos = width/2 end
 	if game.players[2] then game.players[2].pos = width/2 end
-end
-
-function game.endGame(playerID)
-	if playerID == 1 then
-		local player = game.players[2]
-		if player then player.score = player.score + 1 end
-	elseif playerID == 2 then
-		local player = game.players[1]
-		if player then player.score = player.score + 1 end
-	end
-	-- love.event.quit()
-	game.startGame()
 end
 
 return game
